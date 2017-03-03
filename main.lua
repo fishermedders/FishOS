22384375193f9c52d50e1402c97527ed622f3065
Fisher <fishermedders@gmail.com>
2017-03-03 12:43:51 -0500

    Update main.lua
    Cleaned up code and variable names -- added path file and global OS path'ing.

--- main.lua
+++ main.lua
@@ -1,6 +1,13 @@
-tUtils = fs.list("/bin/util/")
+--FishOS Main Operating File -- 
+--FishOS is an operating system for
+--ComputerCraft, (computercraft.lua)
+--and uses LUA. This is an example of
+--an operating system that you can
+--build on top of craftos.
+
+tUtils = fs.list( "/bin/util/" )
 for i = 1,#tUtils do
-  os.loadAPI("/bin/util/"..tUtils[i])
+  os.loadAPI( "/bin/util/" .. tUtils[i] )
 end
 
 --Start Environment Variables Setup
@@ -14,37 +21,34 @@ environment = {
   },
   ["colors"]={}
 }
-  --System
-  environment.system.branch = "dev1"
-  environment.system.keys = {nil,"1","2","3","4","5","6","7","8","9","0",nil,nil,nil,nil,"q","w","e","r","t","y","u","i","o","p",nil,nil,nil,nil,"a","s","d","f","g","h","j","k","l",nil,nil,nil,nil,nil,"z","x","c","v","b","n","m"}
-  environment.system.user = nil
-  environment.system.dirs = {"bin","boot","dev","etc","home","lib","lost+found","misc","mnt","net","opt","proc","root","sbin","tmp","usr","var"}
-  --Colors
-  environment.colors.background = colors.black
-  environment.colors.text = colors.white
-  environment.colors.vanity = colors.red
+
+--System
+environment.system.branch = "dev1"
+environment.system.keys = { nil, "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", nil, nil, nil, nil, "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", nil, nil, nil, nil, "a", "s", "d", "f", "g", "h" ,"j", "k", "l", nil, nil, nil, nil, nil, "z", "x", "c", "v", "b", "n", "m" } 
+environment.system.user = nil
+environment.system.dirs = { "bin", "boot", "dev", "etc", "home", "lib", "lost+found", "misc", "mnt", "net", "opt", "proc", "root", "sbin", "tmp", "usr", "var" }
+--Colors
+environment.colors.background = colors.black
+environment.colors.text = colors.white
+environment.colors.vanity = colors.red
 --End Environmental Variable Setup
 
 _G["exitcmd"] = false
 
-users = {"fisher"}
-passwords = {"fishae"}
-
-
 function reset()
-  term.setBackgroundColor(environment.colors.background)
-  term.setTextColor(environment.colors.text)
+  term.setBackgroundColor( environment.colors.background )
+  term.setTextColor( environment.colors.text )
 end
 
-function auth(user,pass)
-  pwfile = fs.open("/etc/shadow","r")
-  tbl = textutils.unserialize(pwfile.readAll())
-  pwfile.close()
-  for i = 1,#tbl do
-  	for u = 1,#tbl[i] do
-  	  if user == tbl[i][1] then
-  	  	if str.SHA1(pass) == tbl[i][2] then
-  		  environment.system.user = user
+function auth( sUser, sPass )
+  local oFile = fs.open( "/etc/shadow", "r" )
+  local tPass = textutils.unserialize( oFile.readAll() )
+  oFile.close()
+  for i = 1,#tPass do
+  	for u = 1,#tPass[i] do
+  	  if sUser == tPass[i][1] then
+  	  	if str.SHA1(sPass) == tPass[i][2] then
+  		  environment.system.user = sUser
   		  return true
   		end
       end
@@ -54,117 +58,120 @@ function auth(user,pass)
 end
 
 function invisread()
-  returnstr = ""
-  term.setCursorBlink(true)
+  local sReturn = ""
+  term.setCursorBlink( true )
   while true do
-    evnt = {os.pullEvent("key")}
-      if evnt[2] == 14 then
-          returnstr = returnstr:sub(1,#returnstr-1)
-      elseif evnt[2] == 28 then
-          break
-      else
-      	if environment.system.keys[evnt[2]] ~= nil then
-          returnstr = returnstr..environment.system.keys[evnt[2]]
-        end
+    tEvent = { os.pullEvent( "key" ) }
+    if tEvent[2] == 14 then
+      sReturn = sReturn:sub( 1, #sReturn-1 )
+    elseif tEvent[2] == 28 then
+      break
+    else
+     	if environment.system.keys[tEvent[2]] ~= nil then
+        sReturn = sReturn .. environment.system.keys[tEvent[2]]
       end
+    end
   end
-  term.setCursorBlink(false)
-  return returnstr
+  term.setCursorBlink( false )
+  return sReturn
 end
 
 function login()
   reset()
-  term.setCursorPos(1,1)
+  term.setCursorPos( 1, 1 )
   term.clear()
-  print("")
-  print("FishOS Core release "..environment.system.branch.." "..os.computerID())
-  print("")
-  write("FOSC "..os.computerID().." login: ")
-  user = read()
-  write("Password: ")
-  pass = invisread()
-  print(" ")
-  tbl = {}
-  table.insert(tbl,user)
-  table.insert(tbl,pass)
-  return tbl
+  print( "" )
+  print( "FishOS Core release " .. environment.system.branch .. " " .. os.computerID() )
+  print( "" )
+  write( "FOSC " .. os.computerID() .. " login: " )
+  local sUser = read()
+  write( "Password: " )
+  local sPass = invisread()
+  print( " " )
+  local tAuth = {}
+  table.insert(tAuth,sUser)
+  table.insert(tAuth,sPass)
+  return tAuth
 end
 
 function dfind( sFile )
   if fs.exists( sFile ) then
-  	if not fs.isDir( sFile ) then
-  	  return true
-  	end
+   	if not fs.isDir( sFile ) then
+  	   return true
+  	 end
   end
   return false
 end
 
-if fs.exists("/etc/shadow") then
-  file = fs.open("/etc/shadow","r")
-  tbl = textutils.unserialize(file.readAll())
-  for i = 1,#tbl do
-    if not fs.isDir("home/"..tbl[i][1]) then
-      fs.makeDir("home/"..tbl[i][1])
+if fs.exists( "/etc/shadow" ) then
+  local oFile = fs.open( "/etc/shadow", "r" )
+  tPass = textutils.unserialize( oFile.readAll() )
+  oFile.close()
+  for i = 1,#tPass do
+    if not fs.isDir( "home/" .. tPass[i][1] ) then
+      fs.makeDir( "home/" .. tPass[i][1] )
     end
   end
-  tbl = nil
+  tPass = nil
 end
 
 for i = 1,#environment.system.dirs do
-  if not fs.isDir(environment.system.dirs[i]) then
-  	fs.makeDir(environment.system.dirs[i])
+  if not fs.isDir( environment.system.dirs[i] ) then
+  	fs.makeDir( environment.system.dirs[i] )
   end
 end
 
-if not fs.exists("/etc/shadow") then
+if not fs.exists( "/etc/shadow" ) then
   term.clear()
-  term.setCursorPos(1,1)
-  print("Welcome to FishOS, the only OS you'll need.")
-  print(" ")
-  print("FishOS has been customized to be very useful on the CL Tekkit server.")
-  print(" ")
-  print("We will now setup your first user account.")
-  print("NOTE: You will be able to add more accounts later by using the command 'addusr'.")
-  print("Also note, the password field will be invisible for security reasons.")
-  term.write("User: ")
-  user = read()
-  term.write("Pass: ")
-  pass = invisread()
-  file = fs.open("/etc/shadow","w")
-  ptbl = {}
-  tbl = {}
-  table.insert(tbl,user)
-  table.insert(tbl,str.SHA1(pass))
-  table.insert(ptbl,tbl)
-  file.write(textutils.serialize(ptbl))
-  file.close()
-  print("User '"..user.."' added. Press any key to Continue!")
-  os.pullEvent("key")
+  term.setCursorPos( 1, 1 )
+  print( "Welcome to FishOS, the only OS you'll need." )
+  print( " " )
+  print( "FishOS has been customized to be very useful on the CL Tekkit server." )
+  print( " " )
+  print( "We will now setup your first user account." )
+  print( "NOTE: You will be able to add more accounts later by using the command 'addusr'." )
+  print( "Also note, the password field will be invisible for security reasons." )
+  term.write( "User: " )
+  local sUser = read()
+  term.write( "Pass: " )
+  local sPass = invisread()
+  local oFile = fs.open( "/etc/shadow", "w" )
+  local tPass = {}
+  local tTmp = {}
+  table.insert( tTmp, sUser )
+  table.insert( tTmp, str.SHA1( sPass ) )
+  table.insert( tPass, tTmp )
+  tTmp = nil
+  oFile.write( textutils.serialize( tPass ) )
+  oFile.close()
+  tPass = nil
+  print( "User '" .. user .. "' added. Press any key to Continue!" )
+  os.pullEvent( "key" )
   os.reboot()
 end
 
 function boot()
   while true do
-  	_G["logout"] = false
-  	l = login()
-    if auth(l[1],l[2]) then
-      print("Welcome to FishOS Core Release "..environment.system.branch.." "..os.computerID())
-      print(" ")
-      print(" * Documentation: /usr/share/doc/")
-      print(" * Support:       XMedders ingame")
-      print(" ")
-      print("[num] Packages can be updated.")
-      print("[num] updates are security updates.")
-      print(" ")
-      print(" ")
-      write("The programs included with the FishOS system are free software; the exact distribution terms for each program are described in the individual files in /usr/share/doc/copyright")
-      print(" ")
-      write("FishOS comes with no warranty, but ask XMedders ingame for a new copy and he will give ya one :D")
-      print(" ")
-      print(" ")
-      print(" ")
-      print("Press any Key to continue")
-      os.pullEvent("key")
+  	 _G["logout"] = false
+  	 l = login()
+    if auth( l[1], l[2] ) then
+      print( "Welcome to FishOS Core Release " .. environment.system.branch .. " " .. os.computerID() )
+      print( " " )
+      print( " * Documentation: /usr/share/doc/" )
+      print( " * Support:       XMedders ingame" )
+      print( " " )
+      print( "[num] Packages can be updated." )
+      print( "[num] updates are security updates." )
+      print( " " )
+      print( " " )
+      write( "The programs included with the FishOS system are free software; the exact distribution terms for each program are described in the individual files in /usr/share/doc/copyright" )
+      print( " " )
+      write( "FishOS comes with no warranty, but ask XMedders ingame for a new copy and he will give ya one :D" )
+      print( " " )
+      print( " " )
+      print( " " )
+      print( "Press any Key to continue" )
+      os.pullEvent( "key" )
       while true do
         if _G["exitcmd"] or _G["logout"] then
           break
@@ -172,28 +179,45 @@ function boot()
         if shell.dir() == "" then
           dir = ""
         else
-          dir = "/"..shell.dir()
+          dir = "/" .. shell.dir()
         end
-        label = ""
+        local sLabel = ""
         if os.getComputerLabel() ~= nil then
-          label = os.getComputerLabel()
+          sLabel = os.getComputerLabel()
         else
-          label = "Comp"
+          sLabel = "Comp"
         end
-        write(environment.system.user.."@"..label.."-"..os.computerID()..":~"..dir.."$ ")
-        console = read(nil, environment.system.terminal.cmds)
-        table.insert(environment.system.terminal.cmds,console)
-        found = false
-        if console:gsub(" ","") ~= "" then
-          args = {}
-		  for arg in console:gmatch("%w+") do table.insert(args, arg) end
-          if dfind("/bin/"..args[1]) then
-          	found = true
-          	shell.run("/bin/"..console)
+        write( environment.system.user .. "@" .. sLabel .. "-" .. os.computerID() .. ":~" .. dir .. "$ " )
+        local sInput = read( nil, environment.system.terminal.cmds )
+        table.insert( environment.system.terminal.cmds, sInput )
+        local bFound = false
+        if sInput:gsub( " ", "" ) ~= "" then
+          local tArgs = {}
+          if sInput:find( " " ) then
+		          for sArg in sInput:gmatch( "%S+" ) do
+              table.insert( tArgs, sArg )
+            end
+          else
+            tArgs[1] = sInput
+          end
+          local oFile = fs.open( "/var/path/environment.pth", "r" )
+          local tPath = textutils.unserialise( oFile.readAll() )
+          oFile.close()
+          local sArgs = ""
+          if #tArgs >= 2 then
+            for i = 2,#tArgs do
+              sArgs = sArgs .. tArgs[i] .. " "
+            end
+          end
+          for i = 1,#tPath do
+            if dfind( tPath[i] .. "/" .. tArgs[1] .. ".lua" ) then
+             	bFound = true
+             	shell.run( tPath[i] .. "/" .. tArgs[1] .. ".lua " .. sArgs )
+            end
           end
         end
-        if not found then
-          shell.run(console)
+        if not bFound then
+          shell.run(sInput)
         end
         reset()
       end
