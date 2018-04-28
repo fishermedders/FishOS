bf88b8328bb311d78b60c6adc880729c469e0427
Fisher <fishermedders@gmail.com>
2018-04-28 10:50:00 -0400

    Update update.lua

--- update.lua
+++ update.lua
@@ -1,26 +1,35 @@
-tUrls = {"vd7rG2Ue","Wc9ia7Kr","vt3hV9kC","PYWjbzxb","K2aPQ2zp","ZxLvG9z4","x1ybNtea","MmGY8T2U","ycV50AzP","uxFQZyTV","8FrqEbLJ"}
+tUrls = {"/main.lua","/bin/list.lua","/bin/nano.lua","/bin/util/str.lua","/bin/logout.lua","/bin/cardboard.lua","/bin/addusr.lua","/bin/passwd.lua","/bin/util/fishutils.lua","/usr/share/doc/copyright.txt","/update.lua"}
 tDirs = {"/bin","/bin/util","usr/share/doc"}
-tFiles = {{"/startup"},{"/bin/list","/bin/ls"},{"/bin/nano"},{"/bin/util/str"},{"/bin/logout"},{"/bin/cardboard"},{"/bin/addusr"},{"/bin/passwd"},{"/bin/util/fishutils"},{"usr/share/doc/copyright"},{"/bin/update"}}
+tFiles = {{"/main"},{"/bin/list"},{"/bin/nano"},{"/bin/util/str"},{"/bin/logout"},{"/bin/cardboard"},{"/bin/addusr"},{"/bin/passwd"},{"/bin/util/fishutils"},{"usr/share/doc/copyright"},{"/bin/update"}}
+sFSBase = "https://raw.githubusercontent.com/fishermedders/FishOS/master"
 
 for i = 1,#tDirs do
   fs.makeDir(tDirs[i])
 end
 
---No longer needed, we host on github now :)
---[[for i = 1,#tUrls do
+for i = 1,#tUrls do
   for i1 = 1,#tFiles[i] do
-    fs.delete(tFiles[i][i1])
-    shell.run("pastebin get "..tUrls[i].." "..tFiles[i][i1])
+    oFile = fs.open(tFIles[i][i1],"w")
+    print("Grabbing File: " .. tUrls[i] .. "from github!")
+    sContent = http.get( sFSBase .. tUrls[i] ).readAll()
+    oFile.write(sContent)
+    oFile.close()
+    --fs.delete(tFiles[i][i1])
+    --shell.run("pastebin get "..tUrls[i].." "..tFiles[i][i1])
     print("Updated file "..tFiles[i][i1])
   end
-end]]--
+end
 
 if fs.exists("/startup") then
   fs.move("/startup","/startup_old.fos")
 end
 
-oFile = fs.open( "startup", "w" )
-oFile.write( "shell.run(\"main.lua\")" )
+oFile = fs.open("startup", "w")
+oFile.write("shell.run(\"main.lua\")")
+oFile.close()
+
+oFile = fs.open(".startinstruct", "w")
+oFile.write("fs.delete(\"update.lua\")")
 oFile.close()
 
 if _G["isdisk"] == nil then
